92ea1091252bc083489a82414014537bc3288f5a
Helo B <mhbbenthiem@gmail.com>
2026-08-03 14:38:18 -0300

    fix: ajustando tsconfig para aceitar server.ts na raiz

--- backend/server.ts
+++ backend/server.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import funcionarioRoutes from './routes/funcionarioRoutes';
+import funcionarioRoutes from './src/routes/funcionarioRoutes';
 
 dotenv.config();
 
